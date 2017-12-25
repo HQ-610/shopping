da60e6ab13179915bf0cf55980ead287a2ab09d0
jasminehou <hjh892084961@163.com>
2017-12-25 15:50:47 +0800

    test

--- travis.sh
+++ travis.sh
@@ -1,15 +1,15 @@
 
-# echo git tag: $TRAVIS_TAG
-# if [ $TRAVIS_TAG ] && [ "$TRAVIS_TAG"x != ""x ]; then
-PURE_TAG=`git status | grep 'Not currently on any branch'`
-CUR_SHA=`git rev-parse HEAD`
-CUR_TAG=`git tag --points-at $CUR_SHA`
+echo git tag: $TRAVIS_TAG
+if [ $TRAVIS_TAG ] && [ "$TRAVIS_TAG"x != ""x ]; then
+# PURE_TAG=`git status | grep 'Not currently on any branch'`
+# CUR_SHA=`git rev-parse HEAD`
+# CUR_TAG=`git tag --points-at $CUR_SHA`
 
-echo ''
-echo git status: $PURE_TAG
-echo git tag: $CUR_TAG
+# echo ''
+# echo git status: $PURE_TAG
+# echo git tag: $CUR_TAG
 
-if [ $CUR_TAG ] && [ "$PURE_TAG"x != ""x ]; then
+# if [ $CUR_TAG ] && [ "$PURE_TAG"x != ""x ]; then
 
 echo ''
 echo '[is a tag] start packing'
