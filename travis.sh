220cb0b430a81f986da417fb3d1803522c1a5b28
jasminehou <hjh892084961@163.com>
2017-12-28 11:42:08 +0800

    test

--- travis.sh
+++ travis.sh
@@ -3,15 +3,21 @@ echo git tag: $TRAVIS_TAG
 
 if [ $TRAVIS_TAG ] && [ "$TRAVIS_TAG"x != ""x ]  then
 
+    echo "hh"
+    echo `echo $TRAVIS_TAG | grep "^plugin_[0-9]+\.[0-9]+\.[0-9]+$"
+
     if [`echo $TRAVIS_TAG | grep "^plugin_[0-9]+\.[0-9]+\.[0-9]+$"`] then
 
         echo ''
         echo '[is a tag] start packing'
         npm install
         npm run build
+
     else
+
         echo ''
         echo 'The format of the tag is not correct'
+
     fi
 
 else
